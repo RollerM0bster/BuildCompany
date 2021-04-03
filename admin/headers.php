e3fe00a99a11e0348284b8f077196325aee8d68e
DuKiri <dukiri@yandex.ru>
2021-04-03 15:05:35 +0300

    change headers for CORS

--- admin/headers.php
+++ admin/headers.php
@@ -1,4 +1,8 @@
 <?php
-header('Access-Control-Allow-Headers: Access-Control-Allow-Origin, Content-Type');
+header('Accept: application/json, text/plain, */*');
+header('Access-Encoding: gzip, deflate');
 header('Access-Control-Allow-Origin: *');
-header('Content-Type: application/json, charset=utf-8');
+header('Access-Control-Max-Age: 1000');
+header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
+
